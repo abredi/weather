7c7db6e2b633f237737ecff6749129aab49a0194
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-11-29 00:18:21 +0300

    add weather card

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import greetingModule from "./dom";
 
 const init = () => {
   const gm = greetingModule();
-  const mainContent = document.getElementById('content');
-  mainContent.classList.add(...['flex', 'justify-center'])
-  mainContent.appendChild(gm.createGreeting('Hello World'));
+  // const mainContent = document.getElementById('content');
+  // mainContent.classList.add(...['flex', 'justify-center'])
+  // mainContent.appendChild(gm.createGreeting('Hello World'));
 };
 
 init();
