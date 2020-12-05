a843281a5950870d7601b967f0ccade49344b069
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-12-05 06:33:44 +0300

    fix yarn/npm glitches

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ const apiModule = () => {
   };
 
   const post = async (uri = '', data = {}) => {
-    const response = await fetch(`${BASE_URL + uri}&appid=${KEY}`, {
+    const resp = await fetch(`${BASE_URL + uri}&appid=${KEY}`, {
       method: 'POST',
       mode: 'cors',
       cache: 'no-cache',
@@ -22,30 +22,20 @@ const apiModule = () => {
       redirect: 'follow',
       referrerPolicy: 'no-referrer',
       body: JSON.stringify(data),
-    }).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      return Promise.reject(response);
     }).catch(handleError);
 
-    if (response) {
-      return response;
+    if (resp) {
+      return resp.json();
     }
     return false;
   };
 
   const get = async (uri) => {
-    const req = await fetch(`${BASE_URL + uri}&appid=${KEY}`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        return Promise.reject(response);
-      }).catch(handleError);
+    const resp = await fetch(`${BASE_URL + uri}&appid=${KEY}`)
+      .catch(handleError);
 
-    if (req) {
-      return req;
+    if (resp) {
+      return resp.json();
     }
     return false;
   };
